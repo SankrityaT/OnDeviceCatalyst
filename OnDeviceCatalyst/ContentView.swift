a5193b7fddbc69df5e0b5695cb8290325fe01dbf
SankrityaT <sthaku20@asu.edu>
2025-08-28 11:10:05 -0700

    Initial Commit

---
+++ OnDeviceCatalyst/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnDeviceCatalyst
+//
+//  Created by Sankritya Thakur on 8/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnDeviceCatalyst/OnDeviceCatalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceCatalystApp.swift
+//  OnDeviceCatalyst
+//
+//  Created by Sankritya Thakur on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceCatalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
